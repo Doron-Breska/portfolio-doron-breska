93784badf278a8ed134e38647ba86ede62ff2ea9
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2023-09-27 22:14:44 +0200

    new interface vite-env.d.ts

--- src/Components/FbConfig.tsx
+++ src/Components/FbConfig.tsx
@@ -1,10 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import * as dotenv from "dotenv";
-dotenv.config();
 
-const firebaseConfig = import.meta.env.FB_CONFIG;
+const fbConfig = JSON.parse(import.meta.env.VITE_FB_CONFIG as string);
+
+const firebaseConfig = fbConfig;
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,5 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  VITE_FB_CONFIG: string; // JSON string of the configuration object
+}
