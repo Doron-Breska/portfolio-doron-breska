3075b0080e73eb4e5e24b09c30a2737223a3d306
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2023-09-27 22:01:04 +0200

    new FB config

--- src/Components/FbConfig.tsx
+++ src/Components/FbConfig.tsx
@@ -1,15 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCP4LQ_rWw6TG3RCbcdKcYdTb1Rv4uHuPQ",
-  authDomain: "portfolio-fffba.firebaseapp.com",
-  projectId: "portfolio-fffba",
-  storageBucket: "portfolio-fffba.appspot.com",
-  messagingSenderId: "637438110079",
-  appId: "1:637438110079:web:cac87780b6596a1f32124a",
-};
+const firebaseConfig = import.meta.env.FB_CONFIG;
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
