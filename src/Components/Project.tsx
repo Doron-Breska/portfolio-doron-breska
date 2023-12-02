d33261f619f333a2db4ad5c8c940b6f39a2bacd6
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2023-12-02 23:15:35 +0100

    new design ready

--- src/Components/Project.tsx
+++ src/Components/Project.tsx
@@ -17,31 +17,33 @@ const Project: React.FC<ProjectProps> = ({ name, github, pic, tech, url }) => {
     <div className="project-div" style={{ color: " rgb(0, 19, 75)" }}>
       <img
         className="project-pic-div"
-        style={{ width: "100%" }}
+        // style={{ width: "100%" }}
         src={pic}
         alt="project-screenshot"
       />
 
-      <h3>{name}</h3>
-      <p className="technologies">{tech}</p>
-      <a
-        className="con-link"
-        href={github}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FontAwesomeIcon className="faIcon" icon={faGithub} size="2xl" />
-      </a>
-      {url !== "" && (
+      <h3 style={{ marginInline: "0.3rem" }}>{name}</h3>
+      <div style={{ lineHeight: "1.3rem", marginInline: "0.3rem" }}>{tech}</div>
+      <div>
         <a
           className="con-link"
-          href={url}
+          href={github}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <FontAwesomeIcon icon={faGlobe} size="2xl" />
+          <FontAwesomeIcon className="faIcon" icon={faGithub} size="2xl" />
         </a>
-      )}
+        {url !== "" && (
+          <a
+            className="con-link"
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faGlobe} size="2xl" />
+          </a>
+        )}
+      </div>
     </div>
   );
 };
